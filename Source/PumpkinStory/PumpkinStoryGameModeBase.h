f8153c58cd072ff32b2fab8e462d58c05e26184b
Inna <innakhluipo@gmail.com>
2023-10-17 16:29:22 +0300

    MainProject
    Empty base project with default folders

---
+++ Source/PumpkinStory/PumpkinStoryGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PumpkinStoryGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUMPKINSTORY_API APumpkinStoryGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
